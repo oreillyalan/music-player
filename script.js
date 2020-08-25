a7bc6aa79f39f4b8e76d2206b22792dabf195538
oreillyalan <oreillyalan132@gmail.com>
2020-08-25 19:39:17 +0100

    added functionality to progress bar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -9,29 +11,29 @@ const nextBtn = document.getElementById('next');
 
 // Music
 const songs = [{
-    name        : 'song-1',
-    displayName : 'Electric Chill Machine',
-    artist      : 'Creative Commons',
-    image       : 'pic-1',
+    name: 'song-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Creative Commons',
+    image: 'pic-1',
 },
 {
-    name        : 'song-2',
-    displayName : 'Seven Nation Army (Remix)',
-    artist      : 'Creative Commons',
-    image       : 'pic-2',
+    name: 'song-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Creative Commons',
+    image: 'pic-2',
 },
 {
-    name        : 'song-3',
-    displayName : 'Goodnight, Disco Queen',
-    artist      : 'Jacinto Design',
-    image       : 'pic-3',
+    name: 'song-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+    image: 'pic-3',
 
 },
 {
-    name        : 'metric-1',
-    displayName : 'Front Row (Remix)',
-    artist      : 'Metric/Jacinto Design',
-    image       : 'metric-1',
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+    image: 'metric-1',
 
 },
 ]
@@ -44,24 +46,24 @@ let songIndex = 0;
 
 
 // Play
-function playSong(){
+function playSong() {
     isPlaying = true;
     music.play();
-    playBtn.classList.replace('fa-play','fa-pause');
-    playBtn.setAttribute('title','Pause')
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause')
 }
 
 // Pause
-function pauseSong(){
+function pauseSong() {
     isPlaying = false;
     music.pause();
-    playBtn.classList.replace('fa-pause','fa-play');
-    playBtn.setAttribute('title','Play')
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play')
 }
 
 
 //Update Dom
-function loadSong(song){
+function loadSong(song) {
     title.textContent = song.displayName;
     artist.textContent = song.artist;
     music.src = `music/${song.name}.mp3`;
@@ -71,7 +73,7 @@ function loadSong(song){
 // Previous Song
 function prevSong() {
     songIndex--;
-    if (songIndex < 0) {songIndex = songs.length-1}
+    if (songIndex < 0) { songIndex = songs.length - 1 }
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -79,16 +81,26 @@ function prevSong() {
 // Next Song
 function nextSong() {
     songIndex++;
-    if (songIndex > songs.length-1) {songIndex = 0}
+    if (songIndex > songs.length - 1) { songIndex = 0 }
     loadSong(songs[songIndex]);
     playSong();
 }
 
-// Event Listeners
-playBtn.addEventListener('click', ()=> (isPlaying ? pauseSong():playSong()));
-prevBtn.addEventListener('click',  prevSong);
-nextBtn.addEventListener('click',  nextSong);
+// Update Progress Bar & Time of Music Playing
+function updateProgressBar(e) {
+    if (isPlaying) {
+        const { duration, currentTime } = e.srcElement;
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+    }
+}
 
+
+// Event Listeners
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
 
 // On Load 
 loadSong(songs[songIndex]);
